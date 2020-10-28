3505f5de5dd5ab79332904fd27c691bba349a512
Supreme Leader <34385890+Not-Albert@users.noreply.github.com>
2020-10-28 19:16:03 +0000

    up

--- nodes.py
+++ nodes.py
@@ -4,9 +4,25 @@ class Node():
     > have a wheight
     > have a list of all inbound nodes
     """
-    def __init__(self, wheight: float, inboundNodes: list) -> None:
-        self.wheight = wheight
+    import random
+    def __init__(self, inboundNodes: list) -> None:
         self.inboundNodes = inboundNodes
+        self.inboundNodesWeight = [Node.random.Random() for x in inboundNodes]
+        self.value = 0.0
+
+    def wheight(self):
+        """
+        returns the node whight
+        """
+        return self.wheight
+    
+    def calc(self):
+        """
+        calculates the node value from inNode + wheight
+        """
+        self.value = 0.0
+        for node, wheight in zip(self.inboundNodes, self.inboundNodesWeight):
+            self.value += node*wheight
 
 
 class Layer():
@@ -21,3 +37,9 @@ class Layer():
         add node to list
         """
         self.NodeList.append(node)
+    
+    def listNodes(self):
+        """
+        returns a list with all the node is the layer
+        """
+        return self.NodeList
