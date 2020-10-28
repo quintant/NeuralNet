bf9538dbf3b46347f105f4b63259d8c2a72aa108
Supreme Leader <34385890+Not-Albert@users.noreply.github.com>
2020-10-28 21:15:46 +0000

    up

--- theNet.py
+++ theNet.py
@@ -1,19 +1,23 @@
 import random
 import nodes
 
-inLayer = nodes.inLayer(1)
+inLayer = nodes.inLayer(2)
 
-hiddenLayer = nodes.Layer(3, inLayer)
+hiddenLayer = nodes.Layer(4, inLayer)
 
-outLayer = nodes.Layer(1, hiddenLayer)
+hiddenLayer2 = nodes.Layer(5, hiddenLayer)
+
+outLayer = nodes.Layer(10, hiddenLayer2)
 
 
 for i in range(1,3):
     print(f"::-> {i}")
     inp = float(i)
-    inLayer.newVal([inp])
+    inLayer.newVal([inp, inp/36])
     hiddenLayer.calcL()
     outLayer.calcL()
+    hiddenLayer2.calcL()
     print(inLayer.listValues())
     print(hiddenLayer.listValues())
+    print(hiddenLayer2.listValues())
     print(outLayer.listValues())
