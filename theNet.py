852e595edf054ae1260c77bd91c71bb087f3f92b
Supreme Leader <34385890+Not-Albert@users.noreply.github.com>
2020-10-29 01:28:25 +0000

    add

--- theNet.py
+++ theNet.py
@@ -7,7 +7,9 @@ hiddenLayer = nodes.Layer(7, inLayer)
 
 hiddenLayer2 = nodes.Layer(5, hiddenLayer)
 
-outLayer = nodes.Layer(10, hiddenLayer2)
+hiddenLayer3 = nodes.Layer(2, hiddenLayer2)
+
+outLayer = nodes.Layer(10, hiddenLayer3)
 
 
 
@@ -15,6 +17,7 @@ inp = random.uniform(-6.9, 6.9)
 inLayer.newVal([inp, random.uniform(-9.6, 6.9)])
 hiddenLayer.calcL()
 hiddenLayer2.calcL()
+hiddenLayer3.calcL()
 outLayer.calcL()
 print(inLayer.listValues())
 print(hiddenLayer.listValues())
